5e11815e090a6aff090cee765d0c2c6a9201ec7d
EC2 Default User <ec2-user@ip-172-31-4-211.ec2.internal>
2026-03-04 20:17:52 +0000

    Changes to terraform configuration files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,18 @@
 resource "aws_instance" "EC2-SRVR" {
-  ami                    = "ami-0532be01f26a3de55" # Amazon Linux 2 us-east-1
-  instance_type          = "t3.micro"
+  ami                    = "ami-0f3caa1cf4417e51b" # Amazon Linux 2 us-east-1
+  instance_type          = "t2.medium"
   subnet_id              = data.aws_subnets.default.ids[0]  # First default subnet
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   key_name               = "vockey"
 
   user_data = <<-EOF
     #!/bin/bash
-    yum update -y
-    yum install -y docker
-    systemctl start docker
-    systemctl enable docker
-    usermod -aG docker ec2-user
+    sudo dnf update -y
+    sudo dnf install git -y
+    sudo dnf install docker -y
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo usermod -aG docker ec2-user
   EOF
 
   tags = {
